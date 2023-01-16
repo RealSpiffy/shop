587ffe71efbfa2e47dc92887d65a21d3114d9e0a
Scott Nguyen <scottqmn@gmail.com>
2023-01-16 12:24:09 -0800

    Add fonts and colors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,18 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Syne:wght@600;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
