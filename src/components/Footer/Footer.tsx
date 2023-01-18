b3efc3b55e2359ffd8a0f73f398ecb4c4addc392
Suphasuta Prajamsri <suphprajam95@gmail.com>
2023-01-17 20:59:45 -0800

    desktop footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,25 +8,49 @@ export interface FooterProps {
 export const Footer: React.FC<FooterProps> = ({ linkGroups }) => {
   const year = new Date().getFullYear();
 
+  const content = linkGroups.map((section, i) => {
+    return (
+      <div key={section.label} className={styles.category}>
+        <h2 className={styles.categoryLabel}>{section.label}</h2>
+        <ul key={i} className={styles.categoryList}>
+          {section.links &&
+            section.links.map((x, i) => {
+              return (
+                <li key={i} className={styles.categoryItem}>
+                  <a href={x.href} className={styles.link}>
+                    {x.label}
+                  </a>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+    );
+  });
+
   return (
     <footer className={styles.outer}>
-      <form>
-        <label htmlFor="email" className={styles.formLabel}>
-          Newsletter
-        </label>
-        <div className={styles.form}>
-          <input
-            type="email"
-            id="email"
-            name="userEmail"
-            placeholder="Enter your email"
-            className={styles.formInput}
-          ></input>
-          <button type="submit" className={styles.formButton}>
-            <HiChevronRight />
-          </button>
-        </div>
-      </form>
+      <div className={styles.inner}>
+        <div className={styles.content}>{content}</div>
+        <form className={styles.formWrap}>
+          <label htmlFor="email" className={styles.categoryLabel}>
+            Newsletter
+          </label>
+          <div className={styles.form}>
+            <input
+              type="email"
+              id="email"
+              name="userEmail"
+              placeholder="Enter your email"
+              className={styles.formInput}
+            ></input>
+            <button type="submit" className={styles.formButton}>
+              <HiChevronRight />
+            </button>
+          </div>
+        </form>
+      </div>
+
       <small className={styles.copyright}>© Real Spiffy {year}</small>
     </footer>
   );
