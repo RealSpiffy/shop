5b8d017d02db115cc070b392c1c06f1e197a10bb
Scott Nguyen <scottqmn@gmail.com>
2023-01-19 11:43:22 -0800

    GraphQL setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+
 import { PreviewAlert } from "@/components/PreviewAlert";
 import "@/styles/global.scss";
 
+// TODO: move
+const queryClient = new QueryClient();
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       {pageProps.preview && <PreviewAlert />}
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 };
 
