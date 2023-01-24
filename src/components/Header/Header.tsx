48e47ab6254426fea1326e45b6d3472b0232ab8a
Cathy Dao <cathydao95@gmail.com>
2023-01-24 10:16:31 -0800

    install mui, move button, and change padding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
+import MenuIcon from "@mui/icons-material/Menu";
 import clsx from "clsx";
 import Link from "next/link";
-import { FaBars } from "react-icons/fa";
 import styles from "./Header.module.scss";
 
 export interface HeaderProps {
@@ -39,23 +39,25 @@ export const Header: React.FC<HeaderProps> = ({
           className={clsx(styles.btn, styles.menuBtn)}
           onClick={() => console.log("open side bar")}
         >
-          <FaBars />
+          <MenuIcon />
         </button>
 
         <div className={styles.logo}>Shop</div>
 
         <ul className={clsx(styles.links)}>{primaryContent}</ul>
 
-        <div className={styles.btnContainer}>
-          <ul className={clsx(styles.links)}>{secondaryContent}</ul>
-          <button
-            type="button"
-            className={clsx(styles.btn)}
-            onClick={() => console.log("cart")}
-          >
-            Cart: 0
-          </button>
-        </div>
+        <ul className={clsx(styles.linksContainer)}>
+          <li className={clsx(styles.links)}>{secondaryContent}</li>
+          <li>
+            <button
+              type="button"
+              className={clsx(styles.btn)}
+              onClick={() => console.log("cart")}
+            >
+              Cart: 0
+            </button>
+          </li>
+        </ul>
       </nav>
     </header>
   );
