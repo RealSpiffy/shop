5f6c0dea4b4a79b71dd2443e58af045cc22254dd
Suphasuta Prajamsri <suphprajam95@gmail.com>
2023-03-31 16:38:12 -0700

    Config webpack for next and storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+// const path = require("path");
+// const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -13,4 +16,24 @@ module.exports = {
   },
   staticDirs: ["../public"],
   typescript: { reactDocgen: false },
+
+  webpackFinal: (webpackConfig) => {
+    // This modifies the existing image rule to exclude `.svg` files
+    // since we handle those with `@svgr/webpack`.
+    const imageRule = webpackConfig.module.rules.find((rule) => {
+      if (typeof rule !== "string" && rule.test instanceof RegExp) {
+        return rule.test.test(".svg");
+      }
+    });
+    if (typeof imageRule !== "string") {
+      imageRule.exclude = /\.svg$/;
+    }
+
+    webpackConfig.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return webpackConfig;
+  },
 };
